bc44878388dbcd9359272b7fbd4d5de8b16686b2
azoom-n-t-thuat <n.t.thuat@azoom.jp>
2019-09-10 10:06:57 +0000

    Delete code demo mdc-drawer component

--- components/home-page/contact-logs-notification.vue
+++ components/home-page/contact-logs-notification.vue
@@ -20,31 +20,10 @@
         class="cell -span3desktop"
       />
     </div>
-    <aside
-      id="areas-staffs"
-      class="mdc-drawer -modal -right -expand"
-      @keyup.esc="closeDrawer"
-    >
-      <div class="areas-staffs">
-        <div class="mdc-top-app-bar mdc-top-app-bar-fixed -primary">
-          <div class="row">
-            <section class="section -alignstart">
-              <button class="navicon" @click="closeDrawer">
-                <img class="icon" src="@/assets/svg/close.svg" />
-              </button>
-              <h1 class="title">2週間以上ステータス変更されていない案件</h1>
-            </section>
-          </div>
-        </div>
-      </div>
-    </aside>
-    <div class="mdc-drawer-scrim"></div>
   </div>
 </template>
 
 <script>
-import { sync } from 'vuex-pathify'
-import { MDCDrawer } from '@rsmdc/drawer'
 import { get, dispatch } from 'vuex-pathify'
 import contactLogsInfoCard from '~/components/home-page/contact-logs-info-card.vue'
 export default {
@@ -53,30 +32,14 @@ export default {
   },
   computed: {
     totalItemCount: get('staffs/getTotalItemCount'),
-    contactLogs: get('staffs/getContactLogs'),
-    isShowAreasStaffs: sync('ui/areasStaffs@isShow')
-  },
-  watch: {
-    isShowAreasStaffs(value) {
-      this.searchAreasStaffsDrawer.open = value
-      value
-        ? document.body.classList.add('-notscroll')
-        : document.body.classList.remove('-notscroll')
-    }
+    contactLogs: get('staffs/getContactLogs')
   },
   mounted() {
     dispatch('staffs/getContactLogSummary')
-    this.searchAreasStaffsDrawer = new MDCDrawer(
-      document.getElementById('areas-staffs')
-    )
-    this.searchAreasStaffsDrawer.open = this.isShowAreasStaffs
   },
   methods: {
     checkAllContactStatuses() {
-      this.isShowAreasStaffs = true
-    },
-    closeDrawer() {
-      this.isShowAreasStaffs = false
+      console.log(76, this.contactLogs)
     }
   }
 }
